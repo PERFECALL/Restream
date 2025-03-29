5d224bd1dc0ead0cba9bf2febb1e04be2a7d4692
PERFECTALL <najidpubglover12@gmail.com>
2025-03-29 15:55:06 +0600

    Update restream.js

--- api/restream.js
+++ api/restream.js
@@ -4,14 +4,13 @@ module.exports = async (req, res) => {
   const { id } = req.query;
 
   if (!id) {
-    res.status(400).send('ID parameter is required.');
-    return;
+    return res.status(400).send('ID parameter is required.');
   }
 
-  const urlForDomain = `http://watchindia.net:8880/live/97869/86543/2182.ts`;
+  const urlForDomain = `http://watchindia.net:8880/live/97869/86543/${id}.ts`;
 
   try {
-    // First request to fetch the domain
+    // First request to fetch the domain (allow redirects)
     const responseForDomain = await axios.get(urlForDomain, {
       headers: {
         'User-Agent': 'OTT Navigator/1.6.7.4 (Linux;Android 11) ExoPlayerLib/2.15.1',
@@ -19,13 +18,13 @@ module.exports = async (req, res) => {
         'Connection': 'Keep-Alive',
         'Accept-Encoding': 'gzip',
       },
-      maxRedirects: 0, // We don't want to follow redirects automatically
+      // Remove maxRedirects: 0 to allow following redirects automatically
     });
 
     const locationUrl = responseForDomain.headers['location'];
 
     if (!locationUrl) {
-      return res.status(500).send('Error extracting location URL');
+      return res.status(500).send('Error extracting location URL.');
     }
 
     const domain = new URL(locationUrl).host;
@@ -48,6 +47,7 @@ module.exports = async (req, res) => {
 
     res.send(modifiedResponseTextForTs);
   } catch (error) {
+    console.error("Error details:", error); // This will show the error in the logs
     if (error.response) {
       if (error.response.status === 403) {
         return res.status(403).send('Error: 403 Forbidden');
